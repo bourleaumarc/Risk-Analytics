0f7889268f809e17eb025b7c0eddc3486d2d0592
Marc Bourleau <bourleaumarc@gmail.com>
2023-11-16 09:58:07 +0100

    organised github

---
+++ script/setup.R
@@ -0,0 +1,58 @@
+#############################################
+## The following loads the needed packages ##
+#############################################
+
+# load the required packages
+packages <- c(
+  "dplyr", # practical 1 & 2 & 3
+  "QRM",  # practical 1
+  "tseries",  # practical 1
+  "nortest",  # practical 1
+  "forecast",  # practical 1
+  "plotly",  # practical 1 & 2
+  "MASS", # practical 1 
+  "ismev", # practical 2 
+  "VGAM", # practical 2 
+  "tidyverse", # practical 2 
+  "plotrix", # practical 2 
+  "scales", # practical 2 
+  "extRemes", # practical 2 
+
+purrr::walk(packages, library, character.only = TRUE)
+
+######################################################
+## The following sets a few option for nice reports ##
+######################################################
+
+# general options
+options(
+  digits = 3,
+  str = strOptions(strict.width = "cut"),
+  width = 69,
+  tibble.width = 69,
+  cli.unicode = FALSE
+)
+
+# ggplot options
+theme_set(theme_light())
+
+# knitr options
+opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  # cache = TRUE,
+  fig.retina = 0.8,
+  # figures are either vectors or 300 dpi diagrams
+  dpi = 300,
+  out.width = "70%",
+  fig.align = "center",
+  fig.width = 6,
+  fig.asp = 0.618,
+  fig.show = "hold",
+  message = FALSE,
+  echo = FALSE
+)
+
+t <- list(
+  family = "Times New Roman",
+  size = 14)
